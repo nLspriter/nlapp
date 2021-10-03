f460cb4791a8e1adfb3af470030df1a3d13a6c5d
nLspriter <52107943+GACastro95@users.noreply.github.com>
2021-10-03 09:36:31 +0100

    Remove comment

--- lib/flavor.dart
+++ lib/flavor.dart
@@ -1,7 +1,6 @@
 /// Contains the hard-coded settings per flavor.
 class FlavorSettings {
   final String apiBaseUrl;
-  // TODO Add any additional flavor-specific settings here.
 
   FlavorSettings.dev()
       : apiBaseUrl = 'https://nl-app-server-dev.herokuapp.com/status';
